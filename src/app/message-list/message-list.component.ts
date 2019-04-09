886622f4817f8604f3725a32ac0379a0afb3641a
willehpeh <will_alexander@me.com>
2019-04-09 22:08:41 +0200

    Custom properties

--- src/app/message-list/message-list.component.ts
+++ src/app/message-list/message-list.component.ts
@@ -7,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessageListComponent implements OnInit {
 
-  messageArray: { title: string, message: string }[] = [
-    { title: 'First title', message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna' },
-    { title: 'Second title', message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna'},
-    { title: 'Third title', message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna'}
+  messageArray: { title: string, message: string, viewable: boolean }[] = [
+    { title: 'First title',
+      message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna',
+      viewable: true
+    },
+    { title: 'Second title',
+      message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna',
+      viewable: false
+    },
+    { title: 'Third title',
+      message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna',
+      viewable: true
+    }
   ];
 
   constructor() { }
--- src/app/message-preview/message-preview.component.ts
+++ src/app/message-preview/message-preview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-message-preview',
@@ -7,17 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessagePreviewComponent implements OnInit {
 
-  title: string;
+  @Input() title: string;
+  @Input() message: string;
   subtitle: string;
-  messageViewable: boolean;
+  @Input() messageViewable: boolean;
   titleColor: string;
   titleStyle: {};
 
-  constructor() { }
+  constructor() {
+    this.messageViewable = false;
+  }
 
   ngOnInit() {
-    this.title = 'Welcome!';
-    this.messageViewable = Math.random() >= 0.4;
     this.titleColor = 'dodgerblue';
     this.titleStyle = { color: 'peru' };
   }
