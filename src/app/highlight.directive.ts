a8dca9ec4d27672a88695dbed3eb8ad5fef27c5e
willehpeh <will_alexander@me.com>
2019-04-14 14:04:06 +0200

    Basic hover highlight

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,14 +1,22 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective implements OnInit {
 
+  @HostListener('mouseenter') mouseenter($event: Event) {
+    this.renderer.setStyle(this.element.nativeElement, 'background-color', 'yellow');
+  }
+
+  @HostListener('mouseleave') mouseleave($event: Event) {
+    this.renderer.setStyle(this.element.nativeElement, 'background-color', 'transparent');
+  }
+
   constructor(private element: ElementRef,
               private renderer: Renderer2) { }
 
   ngOnInit() {
-    this.renderer.setStyle(this.element.nativeElement, 'background-color', 'yellow');
+
   }
 }
